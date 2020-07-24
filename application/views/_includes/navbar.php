1ea71abe29dbb5cce43b674706a20f2043bc9fe6
adprm <aditiyaprmn00@gmail.com>
2020-07-24 10:58:17 +0700

    set title web in navbar

--- application/views/_includes/navbar.php
+++ application/views/_includes/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
     <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><i class="fab fa-teamspeak"></i> Pengaduan Masyarakat</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><i class="fab fa-teamspeak"></i> <?php echo SITE_NAME ?></a>
     </div>
 </nav>
