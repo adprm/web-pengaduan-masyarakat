877d7f24b498dfe89689012fc9707ee77433d355
adprm <aditiyaprmn00@gmail.com>
2020-07-24 10:58:44 +0700

    set copyright footer

--- application/views/_includes/footer.php
+++ application/views/_includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-light py-5">
     <div class="container">
-        <div class="small text-center text-muted">Copyright © 2020 - Pengaduan Masyarakat</div>
+        <div class="small text-center text-muted">Copyright © 2020 - <?php echo SITE_NAME ?></div>
     </div>
 </footer>
