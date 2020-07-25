fbffb4ddafc755c4fe4ca6665330d349ecf74239
adprm <aditiyaprmn00@gmail.com>
2020-07-25 10:32:45 +0700

    used toast for notification

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,14 +9,25 @@
     <!-- Navigation-->
     <?php $this->load->view("_includes/navbar.php") ?>
     <!-- Masthead-->
+    <?php if ($this->session->flashdata('warning')): ?>
+        <div aria-live="polite" aria-atomic="true">
+            <div class="toast" data-animation="true" data-delay="5000" style="position: absolute; top: 0; right: 10s;">
+                <div class="toast-header">
+                    <span class="rounded mr-2 bg-info" style="width: 15px;height: 15px"></span>
+                    <strong class="mr-auto">Notifikasi</strong>
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+			            <span aria-hidden="true">&times;</span>
+		            </button>
+                </div>
+                <div class="toast-body">
+                    <?php echo $this->session->flashdata('warning'); ?>
+                </div>
+            </div>
+        </div>
+	<?php endif; ?>
     <?php $this->load->view("_includes/header.php") ?>
     <!-- form report-->
     <section class="page-section" id="reports">
-        <?php if ($this->session->flashdata('warning')): ?>
-            <div class="alert alert-warning" role="alert">
-            	<?php echo $this->session->flashdata('warning'); ?>
-            </div>
-		<?php endif; ?>
         <div class="container">
             <h2 class="text-center mt-0">Isi Formulir Laporan</h2>
             <hr class="divider my-4 bg-primary" />
@@ -106,11 +117,14 @@
     <?php $this->load->view("_includes/footer.php") ?>
 	<!-- js -->
     <?php $this->load->view("_includes/js.php") ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+    <script>
+		$('.toast').toast('show');
+	</script>
 </body>
-
-<script>
-    $('.toast').toast(option)
-</script>
 
 <style>
     .report {
