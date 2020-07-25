5daf9b4fb5f81732a166313e2710dd625b3f877f
adprm <aditiyaprmn00@gmail.com>
2020-07-25 13:53:10 +0700

    set variable constant

--- application/views/_includes/contact.php
+++ application/views/_includes/contact.php
@@ -1,20 +1,19 @@
 <div class="row justify-content-center">
     <div class="col-lg-8 text-center">
-        <h2 class="mt-0">Hubungi Kami</h2>
+        <h2 class="mt-0"><?php echo TITLE_CONTACT ?></h2>
         <hr class="my-4 bg-warning" />
-        <p class="text-white mb-5">Kami siap melayani Masyarakat 24 Jam, Jaga keamanan dan ketertiban
-            lingkungan Masyarakat.</p>
+        <p class="text-white mb-5"><?php echo CAPT_TITLE ?></p>
     </div>
 </div>
 <div class="row text-white">
     <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
         <i class="fas fa-phone fa-3x mb-3 text-white"></i>
-        <div>+1 (555) 123-4567</div>
+        <div><?php echo NUMBER_PHONE ?></div>
     </div>
     <div class="col-lg-4 mr-auto text-center">
         <i class="fas fa-envelope fa-3x mb-3 text-white"></i>
         <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
-        <a class="d-block text-white" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+        <a class="d-block text-white" href="mailto:aditiyaprmn00@gmail.com"><?php echo GMAIL ?></a>
     </div>
 </div>
 
