f2d08e3daf10ccdfdc063f7da77c0565fe493470
adprm <aditiyaprmn00@gmail.com>
2020-07-25 14:34:44 +0700

    use animated

--- application/views/_includes/header.php
+++ application/views/_includes/header.php
@@ -2,14 +2,16 @@
     <div class="container h-100">
         <div class="row h-100 align-items-center justify-content-center text-center">
             <div class="col-lg-10 align-self-end">
-                <h1 class="text-uppercase text-white font-weight-bold"><?php echo CAPT_HEADER ?></h1>
+                <h1 class="text-uppercase text-white font-weight-bold animate__animated animate__fadeInUp"><?php echo CAPT_HEADER ?></h1>
                 <hr class="divider my-4" />
             </div>
             <div class="col-lg-8 align-self-baseline">
-                <p class="text-white-50 font-weight-light mb-5">
+                <p class="text-white-50 font-weight-light mb-5 animate__animated animate__fadeInUp">
                     <?php echo DESC_HEADER ?>
                 </p>
-                <a class="btn btn-warning btn-xl js-scroll-trigger" href="#reports"><?php echo BTN_HEADER ?></a>
+                <a class="btn btn-warning btn-xl js-scroll-trigger animate__animated animate__fadeInUp" href="#reports">
+                    <?php echo BTN_HEADER ?>
+                </a>
             </div>
         </div>
     </div>
