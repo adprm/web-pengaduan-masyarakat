e75ca210860335bdb0b93cc95db220d787f78139
adprm <aditiyaprmn00@gmail.com>
2020-07-25 13:37:28 +0700

    set define variable contstant

--- application/views/_includes/header.php
+++ application/views/_includes/header.php
@@ -2,14 +2,14 @@
     <div class="container h-100">
         <div class="row h-100 align-items-center justify-content-center text-center">
             <div class="col-lg-10 align-self-end">
-                <h1 class="text-uppercase text-white font-weight-bold">Menuju Lingkungan Yang Lebih Baik</h1>
+                <h1 class="text-uppercase text-white font-weight-bold"><?php echo CAPT_HEADER ?></h1>
                 <hr class="divider my-4" />
             </div>
             <div class="col-lg-8 align-self-baseline">
                 <p class="text-white-50 font-weight-light mb-5">
-                    Situs Web pengaduan Masyarakat. Laporkan sesuatu yang membuat keresehan pada lingkungan Warga Desa.
+                    <?php echo DESC_HEADER ?>
                 </p>
-                <a class="btn btn-warning btn-xl js-scroll-trigger" href="#reports">Laporkan!</a>
+                <a class="btn btn-warning btn-xl js-scroll-trigger" href="#reports"><?php echo BTN_HEADER ?></a>
             </div>
         </div>
     </div>
