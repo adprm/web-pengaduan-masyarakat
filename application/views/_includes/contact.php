a7e36a4d9035f982268140c4cd4e375574fc272a
adprm <aditiyaprmn00@gmail.com>
2020-07-25 11:24:19 +0700

    styled hr

--- application/views/_includes/contact.php
+++ application/views/_includes/contact.php
@@ -1,12 +1,12 @@
 <div class="row justify-content-center">
     <div class="col-lg-8 text-center">
         <h2 class="mt-0">Hubungi Kami</h2>
-        <hr class="divider my-4" />
+        <hr class="my-4 bg-warning" />
         <p class="text-white mb-5">Kami siap melayani Masyarakat 24 Jam, Jaga keamanan dan ketertiban
             lingkungan Masyarakat.</p>
     </div>
-            </div>
-            <div class="row text-white">
+</div>
+<div class="row text-white">
     <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
         <i class="fas fa-phone fa-3x mb-3 text-white"></i>
         <div>+1 (555) 123-4567</div>
@@ -17,3 +17,10 @@
         <a class="d-block text-white" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
     </div>
 </div>
+
+<style>
+    hr {
+        max-width: 3.25rem;
+        border-width: 0.2rem;
+    }
+</style>
