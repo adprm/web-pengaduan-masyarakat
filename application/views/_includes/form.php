82ffe862102df24e1405873cf08e61df7e9b7bf9
adprm <aditiyaprmn00@gmail.com>
2020-07-25 11:13:15 +0700

    use aos for animation

--- application/views/_includes/form.php
+++ application/views/_includes/form.php
@@ -1,8 +1,8 @@
-<div class="container report">
-    <form action="<?php echo site_url('welcome/add') ?>" method="post" enctype="multipart/form-data">
+<div class="container report" data-aos="fade-up" data-aos-duration="1000">
+    <form action="<?php echo site_url('welcome/add') ?>" method="post" enctype="multipart/form-data" data-aos="fade-UP">
         <div class="form-row pt-4">
             <!-- nik -->
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-6" data-aos="fade-right" data-aos-duration="1000">
                 <input type="number" class="form-control <?php echo form_error('report_nik') ? 'is-invalid':'' ?>" 
                 name="report_nik" placeholder="Ketik NIK Anda *">
                 <div class="invalid-feedback">
@@ -10,7 +10,7 @@
                 </div>
             </div>
             <!-- name -->
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-6" data-aos="fade-left" data-aos-duration="1000">
                 <input type="text" class="form-control <?php echo form_error('report_name') ? 'is-invalid':'' ?>" 
                 name="report_name" placeholder="Ketik Nama Anda *">
                 <div class="invalid-feedback">
@@ -20,14 +20,14 @@
         </div>
         <!-- RT RW -->
         <div class="form-row">
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-6" data-aos="fade-right" data-aos-duration="1000">
                 <input type="number" class="form-control <?php echo form_error('report_rt') ? 'is-invalid':'' ?>" 
                 name="report_rt" placeholder="RT *">
                 <div class="invalid-feedback">
 					<?php echo form_error('report_rt') ?>
 				</div>
             </div>
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-6" data-aos="fade-left" data-aos-duration="1000">
                 <input type="number" class="form-control <?php echo form_error('report_rw') ? 'is-invalid':'' ?>" 
                 name="report_rw" placeholder="RW *">
                 <div class="invalid-feedback">
@@ -36,7 +36,7 @@
             </div>
         </div>
         <!-- title -->
-        <div class="form-group">
+        <div class="form-group" data-aos="fade-up" data-aos-duration="1000">
             <input type="text" class="form-control <?php echo form_error('report_title') ? 'is-invalid':'' ?>" 
             name="report_title" placeholder="Ketik Judul Laporan *">
             <div class="invalid-feedback">
@@ -44,7 +44,7 @@
 			</div>
         </div>
         <!-- desc -->
-        <div class="form-group">
+        <div class="form-group" data-aos="fade-up" data-aos-duration="1000">
             <textarea class="form-control <?php echo form_error('report_desc') ? 'is-invalid':'' ?>"
             name="report_desc" rows="6" placeholder="Ketik Isi Laporan Anda... *"></textarea>
             <div class="invalid-feedback">
@@ -52,7 +52,7 @@
             </div>
         </div>
         <!-- date -->
-        <div class="form-group">
+        <div class="form-group" data-aos="fade-up" data-aos-duration="1000">
             <label for="report_date" class="text-white">Pilih Tanggal Kejadian *</label>
             <input class="form-control <?php echo form_error('report_date') ? 'is-invalid':'' ?>"
              type="date" name="report_date" min="0" placeholder="Tanggal Kejadian" />
@@ -61,7 +61,7 @@
             </div>
         </div>
         <!-- file -->
-        <div class="form-group text-white">
+        <div class="form-group text-white" data-aos="fade-up" data-aos-duration="1000">
             <label for="report_file">Lampiran (Opsional)</label>
             <input class="form-control-file <?php echo form_error('report_file') ? 'is-invalid':'' ?>"
              type="file" name="report_file" />
@@ -69,7 +69,7 @@
             	<?php echo form_error('report_file') ?>
             </div>
 		            </div>
-        <input class="btn btn-warning mb-4" type="submit" name="btn" value="Laporkan!" />
+        <input class="btn btn-warning mb-4" type="submit" name="btn" value="Laporkan!" data-aos="fade-up" data-aos-duration="1000" />
     </form>
 </div>
 
