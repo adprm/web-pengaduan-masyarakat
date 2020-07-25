fa3be2b5aaa52c31a89c7d728a9afea41eb72a39
adprm <aditiyaprmn00@gmail.com>
2020-07-25 14:17:50 +0700

    change link css

--- application/views/_includes/head.php
+++ application/views/_includes/head.php
@@ -11,9 +11,4 @@
 <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic"
 rel="stylesheet" type="text/css" />
-<!-- Third party plugin CSS-->
-<link rel="stylesheet" href=<?php echo base_url("css/magnific-popup.min.css") ?>
-<!-- Core theme CSS (includes Bootstrap)-->
 <link href="<?php echo base_url("css/styles.css") ?>" rel="stylesheet" />
-<!-- aos -->
-<link rel="stylesheet" href="<?php echo base_url("css/aos.css") ?>">
