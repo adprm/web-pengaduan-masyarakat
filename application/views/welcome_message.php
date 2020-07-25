ca0bf7677547d0254e7fb9db7573ba7124cf1426
adprm <aditiyaprmn00@gmail.com>
2020-07-25 10:38:03 +0700

    load component toast

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,22 +9,7 @@
     <!-- Navigation-->
     <?php $this->load->view("_includes/navbar.php") ?>
     <!-- Masthead-->
-    <?php if ($this->session->flashdata('warning')): ?>
-        <div aria-live="polite" aria-atomic="true">
-            <div class="toast" data-animation="true" data-delay="5000" style="position: absolute; top: 0; right: 10s;">
-                <div class="toast-header">
-                    <span class="rounded mr-2 bg-info" style="width: 15px;height: 15px"></span>
-                    <strong class="mr-auto">Notifikasi</strong>
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-			            <span aria-hidden="true">&times;</span>
-		            </button>
-                </div>
-                <div class="toast-body">
-                    <?php echo $this->session->flashdata('warning'); ?>
-                </div>
-            </div>
-        </div>
-	<?php endif; ?>
+    <?php $this->load->view("_includes/toast.php") ?>
     <?php $this->load->view("_includes/header.php") ?>
     <!-- form report-->
     <section class="page-section" id="reports">
