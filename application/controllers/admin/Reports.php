e8360c014699f1323d256ff399d5fd42c384f258
adprm <aditiyaprmn00@gmail.com>
2020-07-27 09:58:59 +0700

    make funtion edit

--- application/controllers/admin/Reports.php
+++ application/controllers/admin/Reports.php
@@ -13,4 +13,22 @@ class Reports extends CI_Controller {
         $data['reports'] = $this->report_model->getAll();
         $this->load->view("admin/report/list", $data);
     }
+
+    public function edit($id = null) {
+        if (!isset($id)) redirect('admin/reports');
+
+        $report = $this->report_model;
+        $validation = $this->form_validation;
+        $validation->ser_rules($report->rules());
+
+        if ($validation->run()) {
+            $report->update();
+            $this->session->set_flashdata('info', 'Data berhasil diperbaharui');
+        }
+
+        $data['report'] = $report->getById($id);
+        if (!$data['report']) show_404();
+
+        $this->load->view("admin/report/edit_form", $data);
+    }
 }
