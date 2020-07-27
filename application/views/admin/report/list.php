3e75c8c5daef625e70265e885da05eabe9269d0f
adprm <aditiyaprmn00@gmail.com>
2020-07-27 10:12:40 +0700

    make a template list data

---
+++ application/views/admin/report/list.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view("admin/_partials/head.php") ?>
+</head>
+<!-- script modal -->
+<script>
+function deleteConfirm(url){
+	$('#btn-delete').attr('href', url);
+	$('#deleteModal').modal();
+}
+</script>
+
+<body id="page-top">
+    <!-- navbar -->
+	<?php $this->load->view("admin/_partials/navbar.php") ?>
+
+	<div id="wrapper">
+        <!-- sidebar -->
+		<?php $this->load->view("admin/_partials/sidebar.php") ?>
+        <!-- content -->
+		<div id="content-wrapper">
+            <div class="container-fluid">
+                <!-- url breadcrumb -->
+				<?php $this->load->view("admin/_partials/breadcrumb.php") ?>
+				<!-- DataTables -->
+				<div class="card mb-3">
+					<div class="card-header">
+						<a href="<?php echo site_url('admin/learnings/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+					</div>
+					<div class="card-body">
+						<div class="table-responsive">
+							<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+								<thead>
+									<tr>
+										<th>NIK</th>
+										<th>Nama</th>
+										<th>Judul Laporan</th>
+										<th>Tanggal</th>
+                                        <th>Aksi</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach ($learning as $learning): ?>
+									<tr>
+                                        <!-- NIK -->
+										<td width="150">
+											<?php echo $learning->learning_title ?>
+										</td>
+                                        <!-- nama -->
+                                        <td width="150">
+											<?php echo $learning->learning_title ?>
+										</td>
+                                        <!-- judul -->
+                                        <td width="150">
+											<?php echo $learning->learning_title ?>
+										</td>
+                                        <!-- Tanggal -->
+                                        <td width="150">
+											<?php echo $learning->learning_title ?>
+										</td>
+                                        <!-- event -->
+										<td width="250">
+											<a href="<?php echo site_url('admin/learnings/edit/'.$learning->learning_id) ?>"
+											 class="btn btn-small"><i class="fas fa-edit"></i> Edit</a>
+											<a onclick="deleteConfirm('<?php echo site_url('admin/learnings/delete/'.$learning->learning_id) ?>')"
+											 href="#!" class="btn btn-small text-danger"><i class="fas fa-trash"></i> Delete</a>
+										</td>
+									</tr>
+									<?php endforeach; ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /.container-fluid -->
+			<!-- Sticky Footer -->
+			<?php $this->load->view("admin/_partials/footer.php") ?>
+		</div>
+		<!-- /.content-wrapper -->
+	</div>
+	<!-- /#wrapper -->
+
+<?php $this->load->view("admin/_partials/scrolltop.php") ?>
+<?php $this->load->view("admin/_partials/modal.php") ?>
+<?php $this->load->view("admin/_partials/js.php") ?>
+
+</body>
+
+</html>
