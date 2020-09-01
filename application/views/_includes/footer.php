f405c4d4ace29fd1434ba45edb016142b215a0c7
adprm <aditiyaprmn00@gmail.com>
2020-09-02 03:01:18 +0700

    deleted files

--- application/views/_includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-light py-5">
-    <div class="container">
-        <div class="small text-center text-muted">Copyright © 2020 - <?php echo SITE_NAME ?></div>
-    </div>
-</footer>
--- application/views/_includes/navbar.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><i class="fab fa-teamspeak"></i> <?php echo SITE_NAME ?></a>
-    </div>
-</nav>
