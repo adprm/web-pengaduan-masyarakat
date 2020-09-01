db74801a7aff582589496a48fa0539cd98b6e8a9
adprm <aditiyaprmn00@gmail.com>
2020-09-02 05:36:13 +0700

    load view registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,10 +5,19 @@ class Auth extends CI_Controller {
 
     public function index()
     {
-        $data['title'] = 'Login Page';
+        $data['title'] = 'Halaman Masuk';
 
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login', $data);
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registration()
+    {
+        $data['title'] = 'Buat Akun!';
+
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/registration', $data);
         $this->load->view('templates/auth_footer');
     }
 
