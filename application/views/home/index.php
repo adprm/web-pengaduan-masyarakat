e3b424e2b41d71141ab039d1713877fc4d5f0a1d
adprm <aditiyaprmn00@gmail.com>
2020-09-02 04:14:47 +0700

    index view home

---
+++ application/views/home/index.php
@@ -0,0 +1,39 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end">
+                <h1 class="text-uppercase text-white font-weight-bold">Sistem Pengaduan Masyarakat</h1>
+                <hr class="divider my-4" />
+            </div>
+            <div class="col-lg-8 align-self-baseline">
+                <p class="text-white-75 font-weight-light mb-5">Laporkan setiap kejadian yang meresahkan dan yang dapat merugikan lingkungan demi lingkungan yang lebih baik.</p>
+                <a class="btn btn-info btn-xl js-scroll-trigger" href="#">Masuk <i class="fas fa-sign-in-alt"></i></a>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Contact-->
+<section class="page-section" id="contact">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 text-center">
+                <h2 class="mt-0">Hubungi</h2>
+                <hr class="divider my-4" />
+                <p class="text-muted mb-5">Siap melayani dan memberi bantuan. Hubungi kami atau kirim email kepada kami dan kami akan segera menghubungi Anda!</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                <div>+62 8558535877</div>
+            </div>
+            <div class="col-lg-4 mr-auto text-center">
+                <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                <a class="d-block" href="mailto:contact@yourwebsite.com">aditiyaprmn00@gmail.com</a>
+            </div>
+        </div>
+    </div>
+</section>
+    
