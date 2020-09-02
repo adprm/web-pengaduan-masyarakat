03e4356fed1a9c2c1caceb1f338f7e275ac8c243
adprm <aditiyaprmn00@gmail.com>
2020-09-03 03:57:44 +0700

    check role

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -38,7 +38,12 @@ class Auth extends CI_Controller {
                         'role_id' => $user['role_id']
                     ];
                     $this->session->set_userdata($data);
-                    redirect('user');
+                    // cek role
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else {
+                        redirect('user');
+                    }
                 }else{
                     // jika gagal
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
