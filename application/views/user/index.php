557f849ad751d940e84009454ee971c93e5604ff
adprm <aditiyaprmn00@gmail.com>
2020-09-03 03:59:08 +0700

    index view user

---
+++ application/views/user/index.php
@@ -0,0 +1,31 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <!-- card profile -->
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $user['name']; ?></h5>
+                    <p class="card-text"><?= $user['email']; ?></p>
+                    <p class="card-text">
+                        <small class="text-muted">Member since <?= date('d F Y' , $user['date_created']); ?>
+                    </small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+      
