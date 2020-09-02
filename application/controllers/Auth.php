9352b9819d6694b4895972a1c9e0857e4acd794c
adprm <aditiyaprmn00@gmail.com>
2020-09-02 07:48:05 +0700

    validation register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -14,11 +14,22 @@ class Auth extends CI_Controller {
 
     public function registration()
     {
-        $data['title'] = 'Buat Akun!';
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[5]|matches[password2]', [
+            'matches' => 'Kata sandi tidak sama!',
+            'min_length' => 'Kata sandi terlalu pendek!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[5]|matches[password1]');
 
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration', $data);
-        $this->load->view('templates/auth_footer');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Buat Akun!';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration', $data);
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'data berhasil';
+        }
     }
 
 }
