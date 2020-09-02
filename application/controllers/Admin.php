befefb28eb564142ac67463ac2fb1bc1d5234571
adprm <aditiyaprmn00@gmail.com>
2020-09-03 03:57:20 +0700

    load view

---
+++ application/controllers/Admin.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'Dashboard';
+        
+        $this->load->view('templates/admin_header', $data);
+        $this->load->view('templates/admin_sidebar');
+        $this->load->view('templates/admin_topbar', $data);
+        $this->load->view('admin/index');
+        $this->load->view('templates/admin_footer');
+    }
+
+}
