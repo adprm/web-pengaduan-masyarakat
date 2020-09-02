35f5f0d605bdd8f7b8de2bd5927f623dd7cf6bb5
adprm <aditiyaprmn00@gmail.com>
2020-09-03 03:58:28 +0700

    load view

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,13 @@ class User extends CI_Controller {
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        echo "selamat datang " . $data['user']['name'];
+        $data['title'] = 'My Profile';
+        
+        $this->load->view('templates/admin_header', $data);
+        $this->load->view('templates/admin_sidebar');
+        $this->load->view('templates/admin_topbar', $data);
+        $this->load->view('user/index');
+        $this->load->view('templates/admin_footer');
     }
 
 }
