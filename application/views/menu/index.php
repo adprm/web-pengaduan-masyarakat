85a07da9640e2d23a28577fe1665c61532169d0f
adprm <aditiyaprmn00@gmail.com>
2020-09-05 06:27:37 +0700

    index view menu

---
+++ application/views/menu/index.php
@@ -0,0 +1,73 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="col-lg-7">
+        <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+        <?= $this->session->flashdata('message'); ?>
+    </div>
+
+    <div class="card col-lg-7 shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><a href="" data-toggle="modal" data-target="#newMenuModal"><i class="fas fa-plus"></i> Tambah Menu</a></h6>
+        </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>Menu</th>
+                                <th>Aksi</th>
+                                </tr>
+                        </thead>
+                    <tbody>
+                        <?php $index = 1; ?>
+                        <?php foreach($menu as $m) : ?>
+                            <tr>
+                                <td><?= $index; ?></td>
+                                <td><?= $m['menu']; ?></td>
+                                <td>
+                                    <a class="badge badge-success" href="<?= site_url('menu/editMenu/'.$m['id']); ?>">Ubah</a>
+                                    <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?php echo site_url('menu/deleteMenu/'.$m['id']) ?>')">Hapus</a>
+                                </td>
+                            </tr>
+                        <?php $index++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal add new menu-->
+<div class="modal fade" id="newMenuModal" tabindex="-1" role="dialog" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="newMenuModalLabel">Tambah Menu Baru</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <!-- form -->
+      <form action="<?= site_url('menu/addMenu'); ?>" method="post">
+        <div class="modal-body">
+            <div class="form-group">
+                <input type="text" class="form-control" id="menu" name="menu" placeholder="Nama menu">
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+            <button type="submit" class="btn btn-primary">Tambah</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
