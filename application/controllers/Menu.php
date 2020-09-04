57a3147a0f4235e0a64f8d58f65e29e7852e016c
adprm <aditiyaprmn00@gmail.com>
2020-09-05 06:49:00 +0700

    edit menu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -38,4 +38,31 @@ class Menu extends CI_Controller {
         }
     }
 
+    public function editMenu($id = null)
+    {   
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+        
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Menu Manajemen';
+            $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+            $data['menu'] = $this->db->get_where('user_menu', ['id' => $id])->row_array();
+
+            $this->load->view('templates/admin_header', $data);
+            $this->load->view('templates/admin_sidebar');
+            $this->load->view('templates/admin_topbar', $data);
+            $this->load->view('menu/edit_menu', $data);
+            $this->load->view('templates/admin_footer');
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Gagal mengubah menu!</div>');
+        } else {
+            $post = $this->input->post();
+            $this->id = $post['id'];
+            $this->menu = $post['menu'];
+            $this->db->update('user_menu', $this, ['id' => $post['id']]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Berhasil mengubah menu!</div>');
+            redirect('menu');
+        }
+    }
+
 }
