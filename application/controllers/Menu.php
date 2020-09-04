11ef790b5aa24047c8e50d3e16368362744e3072
adprm <aditiyaprmn00@gmail.com>
2020-09-05 06:27:07 +0700

    method add menu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -16,4 +16,26 @@ class Menu extends CI_Controller {
         $this->load->view('templates/admin_footer');
     }
 
+    public function addMenu()
+    {
+        $data['title'] = 'Menu Manajemen';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/admin_header', $data);
+            $this->load->view('templates/admin_sidebar');
+            $this->load->view('templates/admin_topbar', $data);
+            $this->load->view('menu/index', $data);
+            $this->load->view('templates/admin_footer');
+        } else {
+            $this->db->insert('user_menu', ['menu' => $this->input->post('menu')]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Menu baru ditambahkan!</div>');
+            redirect('menu');
+        }
+    }
+
 }
