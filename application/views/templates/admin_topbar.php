efc02d5cfc420848c3dd4b4c18cb27d40e463f00
adprm <aditiyaprmn00@gmail.com>
2020-09-04 10:26:45 +0700

    modified topbar

--- application/views/templates/admin_topbar.php
+++ application/views/templates/admin_topbar.php
@@ -25,14 +25,14 @@
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="<?= base_url('user'); ?>">
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                        My Profile
+                        Profil Saya
                     </a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Logout
+                        Keluar
                     </a>
                 </div>
                 </li>
