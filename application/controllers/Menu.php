3c760b5f5ebde8c2f5bcfd85d8af11f568db2e4d
adprm <aditiyaprmn00@gmail.com>
2020-09-05 09:59:24 +0700

    delete submenu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -182,4 +182,12 @@ class Menu extends CI_Controller {
         }
     }
 
+    public function deleteSubMenu($id = null)
+    {
+        $this->db->delete('user_sub_menu', ['id' => $id]);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+        Submenu berhasil dihapus!</div>');
+        redirect('menu/submenu');
+    }
+
 }
