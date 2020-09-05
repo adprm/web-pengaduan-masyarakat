fa0bee30a01187bb10892e4ad34baed78a479284
adprm <aditiyaprmn00@gmail.com>
2020-09-06 06:48:20 +0700

    method role

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,13 +11,26 @@ class Admin extends CI_Controller {
 
     public function index()
     {
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         
         $this->load->view('templates/admin_header', $data);
         $this->load->view('templates/admin_sidebar');
         $this->load->view('templates/admin_topbar', $data);
         $this->load->view('admin/index');
+        $this->load->view('templates/admin_footer');
+    }
+
+    public function role()
+    {
+        $data['title'] = 'Akses';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['role'] = $this->db->get('user_role')->result_array();
+        
+        $this->load->view('templates/admin_header', $data);
+        $this->load->view('templates/admin_sidebar');
+        $this->load->view('templates/admin_topbar', $data);
+        $this->load->view('admin/role');
         $this->load->view('templates/admin_footer');
     }
 
