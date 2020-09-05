37aa4451a27b1902d757fd51f09f321d1ad1d8b3
adprm <aditiyaprmn00@gmail.com>
2020-09-06 06:30:51 +0700

    load method blocked

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
