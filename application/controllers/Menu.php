4f0a699c30ede9332f9d542c978067b8d41f3ffc
adprm <aditiyaprmn00@gmail.com>
2020-09-05 08:12:03 +0700

    add submenu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -73,4 +73,54 @@ class Menu extends CI_Controller {
         redirect('menu');
     }
 
+    public function submenu()
+    {
+        $data['title'] = 'Submenu Manajemen';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+        $this->load->model('Menu_model', 'menu');
+        $data['submenu'] = $this->menu->getSubMenu();
+
+        $this->load->view('templates/admin_header', $data);
+        $this->load->view('templates/admin_sidebar');
+        $this->load->view('templates/admin_topbar', $data);
+        $this->load->view('menu/submenu', $data);
+        $this->load->view('templates/admin_footer');
+    }
+
+    public function addSubMenu()
+    {
+        $data['title'] = 'Submenu Manajemen';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+        $this->load->model('Menu_model', 'menu');
+        $data['submenu'] = $this->menu->getSubMenu();
+
+        $this->form_validation->set_rules('title', 'Submenu', 'required');
+        $this->form_validation->set_rules('menu_id', 'Menu', 'required');
+        $this->form_validation->set_rules('url', 'Url', 'required');
+        $this->form_validation->set_rules('icon', 'Icon', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/admin_header', $data);
+            $this->load->view('templates/admin_sidebar');
+            $this->load->view('templates/admin_topbar', $data);
+            $this->load->view('menu/submenu', $data);
+            $this->load->view('templates/admin_footer');
+        } else {
+            $data = [
+                'title' => $this->input->post('title'),
+                'menu_id' => $this->input->post('menu_id'),
+                'url' => $this->input->post('url'),
+                'icon' => $this->input->post('icon'),
+                'is_active' => $this->input->post('is_active')
+            ];
+
+            $this->db->insert('user_sub_menu', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Data submenu berhasil ditambahkan!</div>');
+            redirect('menu/submenu');
+        }
+    }
+
 }
