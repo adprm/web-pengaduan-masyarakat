7b807716e4c7b6ccd5a92b44d5af7a76efd163f2
adprm <aditiyaprmn00@gmail.com>
2020-09-05 07:10:29 +0700

    index view menu

--- application/views/menu/index.php
+++ application/views/menu/index.php
@@ -1,3 +1,10 @@
+<script>
+    function deleteConfirm(url){
+        $('#btn-delete').attr('href', url);
+        $('#deleteModal').modal();
+    }
+</script>
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -30,7 +37,7 @@
                                 <td><?= $m['menu']; ?></td>
                                 <td>
                                     <a class="badge badge-success" href="<?= site_url('menu/editMenu/'.$m['id']); ?>">Ubah</a>
-                                    <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?php echo site_url('menu/deleteMenu/'.$m['id']) ?>')">Hapus</a>
+                                    <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?= site_url('menu/deleteMenu/'.$m['id']); ?>')">Hapus</a>
                                 </td>
                             </tr>
                         <?php $index++; ?>
@@ -68,6 +75,25 @@
             <button type="submit" class="btn btn-primary">Tambah</button>
         </div>
       </form>
+    </div>
+  </div>
+</div>
+
+<!-- modal delete -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content border-left-danger">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apa Anda Yakin?</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Data yang sudah dihapus tidak dapat dikembalikan!</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+        <a id="btn-delete" class="btn btn-danger" href="#">Hapus</a>
+      </div>
     </div>
   </div>
 </div>
