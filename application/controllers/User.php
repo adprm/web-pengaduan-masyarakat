1b0c205d986b7b8cfca3dd3ff97f38d40662e9af
adprm <aditiyaprmn00@gmail.com>
2020-09-06 12:57:15 +0700

    method edit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -11,14 +11,68 @@ class User extends CI_Controller {
 
     public function index()
     {
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['title'] = 'Profil Saya';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         
         $this->load->view('templates/admin_header', $data);
         $this->load->view('templates/admin_sidebar');
         $this->load->view('templates/admin_topbar', $data);
-        $this->load->view('user/index');
+        $this->load->view('user/index', $data);
         $this->load->view('templates/admin_footer');
+    }
+
+    // edit profile
+    public function edit()
+    {
+        $data['title'] = 'Ubah Profil';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('name', 'Nama lengkap', 'required', [
+            'required' => 'Nama lengkap harus diisi!'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/admin_header', $data);
+            $this->load->view('templates/admin_sidebar');
+            $this->load->view('templates/admin_topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/admin_footer');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+
+            // upload image
+            $upload_image = $_FILES['image']['name'];
+            
+            if ($upload_image) {
+                $config['allowed_types']    = 'jpg|jpeg|png';
+                $config['max_size']         = '6000';
+                $config['upload_path']      = './assets/img/profile/';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH.'/assets/img/profile/'.$old_image);
+                    }
+
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->set('name', $name);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Profil berhasil diubah!</div>');
+            redirect('user');
+        }
+        
     }
 
 }
