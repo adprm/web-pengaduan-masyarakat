19453c1951aba9c9a65679b13b3df463596cccbb
adprm <aditiyaprmn00@gmail.com>
2020-09-06 07:35:15 +0700

    index view role

---
+++ application/views/admin/role.php
@@ -0,0 +1,100 @@
+<script>
+    function deleteConfirm(url){
+        $('#btn-delete').attr('href', url);
+        $('#deleteModal').modal();
+    }
+</script>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="col-lg-7">
+        <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+        <?= $this->session->flashdata('message'); ?>
+    </div>
+
+    <div class="card col-lg-7 shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><a href="" data-toggle="modal" data-target="#newRoleModal"><i class="fas fa-plus"></i> Tambah Akses</a></h6>
+        </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>Akses</th>
+                                <th>Aksi</th>
+                                </tr>
+                        </thead>
+                    <tbody>
+                        <?php $index = 1; ?>
+                        <?php foreach($role as $r) : ?>
+                            <tr>
+                                <td><?= $index; ?></td>
+                                <td><?= $r['role']; ?></td>
+                                <td>
+                                    <a class="badge badge-warning" href="<?= site_url('admin/roleaccess/'.$r['id']); ?>">Akses</a>
+                                    <a class="badge badge-success" href="<?= site_url('menu/editMenu/'.$r['id']); ?>">Ubah</a>
+                                    <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?= site_url('menu/deleteMenu/'.$r['id']); ?>')">Hapus</a>
+                                </td>
+                            </tr>
+                        <?php $index++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal add new role-->
+<div class="modal fade" id="newRoleModal" tabindex="-1" role="dialog" aria-labelledby="newRoleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="newRoleModalLabel">Tambah Akses Baru</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <!-- form -->
+      <form action="<?= site_url('admin/role'); ?>" method="post">
+        <div class="modal-body">
+            <div class="form-group">
+                <input type="text" class="form-control" id="role" name="role" placeholder="Nama Akses">
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+            <button type="submit" class="btn btn-primary">Tambah</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- modal delete -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content border-left-danger">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apa Anda Yakin?</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Data yang sudah dihapus tidak dapat dikembalikan!</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+        <a id="btn-delete" class="btn btn-danger" href="#">Hapus</a>
+      </div>
+    </div>
+  </div>
+</div>
