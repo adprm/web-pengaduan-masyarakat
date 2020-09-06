765e6f2012f490782d0621335f7dd147db76ee69
adprm <aditiyaprmn00@gmail.com>
2020-09-06 19:59:00 +0700

    index view change password

---
+++ application/views/auth/change-password.php
@@ -0,0 +1,40 @@
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-lg-7">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900"><?= $title; ?> Untuk</h1>
+                                    <h5 class="mb-4"><?= $this->session->userdata('reset_email'); ?></h5>
+                                </div>
+                                <?= $this->session->flashdata('message'); ?>
+                                <form class="user" method="post" action="<?= base_url('auth/changePassword'); ?>">
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Masukkan kata sandi baru">
+                                        <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Masukkan ulang kata sandi">
+                                        <?= form_error('password2', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Ubah kata sandi
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  </div>
