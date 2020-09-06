fb1a474e0768b3289d695501b0f889fec5ca45bc
adprm <aditiyaprmn00@gmail.com>
2020-09-06 07:36:33 +0700

    ajax

--- application/views/templates/admin_footer.php
+++ application/views/templates/admin_footer.php
@@ -55,6 +55,25 @@
   <!-- Page level custom scripts -->
   <script src="<?= base_url('assets/'); ?>js/demo/datatables-demo.js"></script>
 
+<script>
+$('.form-check-input').on('click', function() {
+    const menuId = $(this).data('menu');
+    const roleId = $(this).data('role');
+
+    $.ajax({
+        url: "<?= base_url('admin/changeaccess'); ?>",
+        type: 'post',
+        data: {
+            menuId: menuId,
+            roleId: roleId
+        },
+        success: function() {
+            document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+        }
+    });
+});
+</script>
+
 </body>
 
 </html>
