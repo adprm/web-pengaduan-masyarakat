a3f3f132e5fc9fc0efbc7999e81ba403c3ed0975
adprm <aditiyaprmn00@gmail.com>
2020-09-06 07:34:51 +0700

    index view role access

---
+++ application/views/admin/role-access.php
@@ -0,0 +1,55 @@
+<script>
+    function deleteConfirm(url){
+        $('#btn-delete').attr('href', url);
+        $('#deleteModal').modal();
+    }
+</script>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <h5>Role : <?= $role['role']; ?></h5>
+    <div class="col-lg-7">
+        <?= $this->session->flashdata('message'); ?>
+    </div>
+
+    <div class="card col-lg-7 shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><a href="<?= base_url('admin/role') ?>"><i class="fas fa-arrow-left"></i> Kembali</a></h6>
+        </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>Menu</th>
+                                <th>Akses</th>
+                                </tr>
+                        </thead>
+                    <tbody>
+                        <?php $index = 1; ?>
+                        <?php foreach($menu as $m) : ?>
+                            <tr>
+                                <td><?= $index; ?></td>
+                                <td><?= $m['menu']; ?></td>
+                                <td>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" <?= check_access($role['id'], $m['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $m['id']; ?>">
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php $index++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
