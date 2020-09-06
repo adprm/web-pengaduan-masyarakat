7bb67cdd63221e859c0430f92ee0b2c74e3d39c8
adprm <aditiyaprmn00@gmail.com>
2020-09-06 14:05:45 +0700

    modified view

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -4,23 +4,25 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-    <div class="col-md-4">
+    <div class="col-md-6">
         <?= $this->session->flashdata('message'); ?>
     </div>
 
-    <!-- card profile -->
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $user['name']; ?></h5>
-                    <p class="card-text"><?= $user['email']; ?></p>
-                    <p class="card-text">
-                        <small class="text-muted">Bergabung sejak <?= date('d F Y' , $user['date_created']); ?>
-                    </small></p>
+    <div class="col-md-6 mb-3">
+        <div class="card shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $user['name']; ?></h5>
+                            <p class="card-text"><?= $user['email']; ?></p>
+                            <p class="card-text">
+                            <small class="text-muted">Bergabung sejak <?= date('d F Y' , $user['date_created']); ?></small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
