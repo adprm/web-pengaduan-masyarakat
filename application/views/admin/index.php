bf6d278aca2d268cd53498a432869bac843a2595
adprm <aditiyaprmn00@gmail.com>
2020-09-08 02:27:31 +0700

    set view total data

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -4,6 +4,89 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Wewenang Akses</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $user_role; ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+                <small><a class="ml-3" href="<?= base_url('admin/role'); ?>">Lihat &rarr;</a></small>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Member</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $user_member; ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+                <small><a class="ml-3" href="#">Lihat &rarr;</a></small>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Menu</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $menu; ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-folder fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+                <small><a class="ml-3" href="<?= base_url('menu'); ?>">Lihat &rarr;</a></small>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Submenu</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $sub_menu; ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-folder-open fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+                <small><a class="ml-3" href="<?= base_url('menu/submenu'); ?>">Lihat &rarr;</a></small>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-dark shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Laporan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $report; ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-tasks fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+                <small><a class="ml-3" href="<?= base_url('report'); ?>">Lihat &rarr;</a></small>
+            </div>
+        </div>
+    </div>
+
 </div>
 <!-- /.container-fluid -->
 
