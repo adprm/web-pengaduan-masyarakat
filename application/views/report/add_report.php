952d2e9ef8ac69a9f9802826084add4b0c8ae5de
adprm <aditiyaprmn00@gmail.com>
2020-09-07 13:05:01 +0700

    index view

---
+++ application/views/report/add_report.php
@@ -0,0 +1,105 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="card shadow border-left-primary mb-3">
+        <div class="card-header">
+        	From Laporkan
+        </div>
+
+        <div class="card-body">
+        	<form action="<?php echo site_url('report/addReport') ?>" method="post" enctype="multipart/form-data" >
+                <div class="form-group">
+                    <label for="title">Nama Pelapor*</label>
+                    <input class="form-control"
+                    type="text" name="name" placeholder="Masukkan nama" value="<?= set_value('name'); ?>">
+                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                    <label for="nik">NIK Pelapor*</label>
+                    <input class="form-control"
+                    type="text" name="nik" placeholder="Masukkan NIK" value="<?= set_value('nik'); ?>">
+                    <?= form_error('nik', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="rt">RT*</label>
+                            <input class="form-control"
+                            type="number" name="rt" placeholder="Masukkan RT" value="<?= set_value('rt'); ?>">
+                            <?= form_error('rt', '<small class="text-danger pl-3">', '</small>'); ?>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="rw">RW*</label>
+                            <input class="form-control"
+                            type="number" name="rw" placeholder="Masukkan RW" value="<?= set_value('rw'); ?>">
+                            <?= form_error('rw', '<small class="text-danger pl-3">', '</small>'); ?>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="village">Desa*</label>
+                            <input class="form-control"
+                            type="text" name="village" placeholder="Desa" value="<?= set_value('village'); ?>">
+                            <?= form_error('village', '<small class="text-danger pl-3">', '</small>'); ?>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="title">Judul Laporan*</label>
+                    <input class="form-control"
+                    type="text" name="title" placeholder="Judul laporan" value="<?= set_value('title'); ?>">
+                    <?= form_error('title', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                    <label for="description">Deskripsi Laporan*</label>
+                    <textarea class="form-control" id="description" name="description" placeholder="Deskripsi laporan" rows="3"></textarea>
+                    <?= form_error('description', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                    <label for="type">Jenis Laporan</label>
+                    <select class="form-control" id="type" name="type">
+                        <option value="Corona Virus">Corona Virus</option>
+                        <option value="Kesehatan">Kesehatan</option>
+                        <option value="Bantuan Pangan">Bantuan Pangan</option>
+                        <option value="Bidang Perekonomian">Bidang Perekonomian</option>
+                        <option value="Energi dan Sumber Daya Alam">Energi dan Sumber Daya Alam</option>
+                        <option value="Pendidikan">Pendidikan</option>
+                        <option value="Pertanian">Pertanian</option>
+                        <option value="Tindak Kriminal">Tindak Kriminal</option>
+                        <option value="Pelecehan Seksual">Pelecehan Seksual</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                <label for="file">Lampiran</label>
+                    <input class="form-control-file"
+                    type="file" name="file" />
+                    <div class="invalid-feedback">
+                        <?php echo form_error('file') ?>
+                    </div>
+                </div>
+                <!-- button save -->
+                <input class="btn btn-primary" type="submit" name="btn" value="Laporkan!" />
+            </form>
+        </div>
+
+        <div class="card-footer small text-muted">
+            * required fields
+        </div>
+	</div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
---
+++ application/views/report/index.php
@@ -0,0 +1,85 @@
+<script>
+    function deleteConfirm(url){
+        $('#btn-delete').attr('href', url);
+        $('#deleteModal').modal();
+    }
+</script>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <?php if (validation_errors()) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?= validation_errors(); ?>
+    </div>
+    <?php endif; ?>
+
+    <?= $this->session->flashdata('message'); ?>
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Laporan</h6>
+        </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nama</th>
+                                    <th>NIK</th>
+                                    <th>Judul Laporan</th>
+                                    <th>Jeni Laporan</th>
+                                    <th>Tanggal</th>
+                                    <th>Aksi</th>
+                                </tr>
+                        </thead>
+                    <tbody>
+                        <?php $index = 1; ?>
+                        <?php foreach($reports as $r) : ?>
+                            <tr>
+                                <td><?= $index; ?></td>
+                                <td><?= $r['name']; ?></td>
+                                <td><?= $r['nik']; ?></td>
+                                <td><?= $r['title']; ?></td>
+                                <td><?= $r['type']; ?></td>
+                                <td><?= date('d F Y' , $r['date_reported']); ?></td>
+                                <td>
+                                    <a class="badge badge-success" href="<?= site_url('report/detail/'.$r['id']); ?>">Detail</a>
+                                    <a class="badge badge-danger" href="#!" onclick="deleteConfirm('<?= site_url('menu/deleteReport/'.$r['id']); ?>')">Hapus</a>
+                                </td>
+                            </tr>
+                        <?php $index++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- modal delete -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apa Anda Yakin?</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Data yang sudah dihapus tidak dapat dikembalikan!</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+        <a id="btn-delete" class="btn btn-danger" href="#">Hapus</a>
+      </div>
+    </div>
+  </div>
+</div>
