c09dba46cf5bd5b160ce347493af1d46a59c600b
adprm <aditiyaprmn00@gmail.com>
2020-09-08 02:27:09 +0700

    data info table

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,13 +11,20 @@ class Admin extends CI_Controller {
 
     public function index()
     {
-        $data['title'] = 'Dashboard';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data = [
+            'title' => 'Dashboard',
+            'user' => $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(),
+            'user_role' => $this->db->get('user_role')->num_rows(),
+            'user_member' => $this->db->get_where('user', ['role_id' => 2])->num_rows(),
+            'menu' => $this->db->get('user_menu')->num_rows(),
+            'sub_menu' => $this->db->get('user_sub_menu')->num_rows(),
+            'report' => $this->db->get('user_report')->num_rows(),
+        ];
         
         $this->load->view('templates/admin_header', $data);
         $this->load->view('templates/admin_sidebar');
         $this->load->view('templates/admin_topbar', $data);
-        $this->load->view('admin/index');
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/admin_footer');
     }
 
