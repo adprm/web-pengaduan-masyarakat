fc725fcba7c0dfbe30cd121acbbfae4e86dc3e54
adprm <aditiyaprmn00@gmail.com>
2020-09-08 02:47:38 +0700

    modified view index

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -26,7 +26,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Member</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Pengguna</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $user_member; ?></div>
                         </div>
                         <div class="col-auto">
